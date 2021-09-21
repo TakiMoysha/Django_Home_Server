d0cdb376a1be0af39140d4ddc27b44f6ca29327e
TakiMoysha <mpwema782@gmail.com>
2021-09-21 21:52:02 +0300

    added forms, view, html templates

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,7 +1,50 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.utils import timezone
+
+from .forms import PostForm
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    context = {}
+    if request.user.is_authenticated:
+        form = PostForm()
+        context['form'] = form
+
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date'),
+    context['posts_list'] = posts
+
+    return render(request, 'blog/post_list.html', context)
+
+
+def post(request):
+    post = Post.objects.get()
+    context = {
+        'post': post
+    }
+    return render(request, 'blog/post_details.html', context)
+
+
+@login_required
+def post_new(request):
+    context = {}
+
+    if request.method == "POST":
+        pass
+    else:
+        form = PostForm()
+        context["form"] = form
+
+    return render(request, 'blog/post_edit.html', context)
+
+
+def home_view(request):
+    return render(request, 'blog/home.html', {})
--- src/home_server/urls.py
+++ src/home_server/urls.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
+from django.http import request
 from django.urls import path
 
-from user.views import *
+from blog.views import post_list, home_view, post, post_new
+
+from user.views import register, profile
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('login/', ...),
+    path('', home_view, name='index'),
+
+    path('admin/', admin.site.urls, name='admin'),
+
+    path('posts/', post_list, name='posts'),
+    path('post/new/', post_new, name='post_new'),
+    path('post/<int:post_index>/', post, name='post_detail'),
     # path('register/', ...),
     # path('profile/', ...),
-    # path('logout/', ...),
-    # path('', ...),
+    path('login/', home_view, name='login'),
+    path('logout/', home_view, name='logout'),
 ]
